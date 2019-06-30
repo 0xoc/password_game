328838542ed9df94afc75821b6124539b1df3eb5
Ali Parvizi <snparvizi75@gmail.com>
2019-07-01 00:35:48 +0430

    Level serialiazer

---
+++ level/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import Level
+
+class LevelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Level
+        fields = [
+            'name', 
+            'time', 
+            'date', 
+            'singer', 
+            'song_name', 
+            'msg_count', 
+            'code', 
+            'notif_sender', 
+            'notif_msg', 
+            'default_phone_number', 
+            'hint_msg', 
+            'incoming_call_number', 
+            'incoming_call_name', 
+            'clipboard_msg', 
+            'second_name', 
+            'second_text', 
+            'hint_1', 
+            'hint_2', 
+            'type', 
+            'image', 
+            'cover', 
+            'incoming_call_image', 
+            'hint_count', 
+            'pin_count', 
+            'index', 
+            'notif', 
+            'hint', 
+            'passed', 
+            'incoming_call', 
+            'clipboard', 
+            'second_notif', 
+        ] 

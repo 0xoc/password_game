a812a7ed40f99bad45985befbfa54d068b6d58cf
Ali Parvizi <snparvizi75@gmail.com>
2019-07-01 01:13:19 +0430

    is up to date

--- level/urls.py
+++ level/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ListLevelsView
+from .views import ListLevelsView, IsUpToDate
 urlpatterns = [
     path('all-levels/', ListLevelsView.as_view()),
+    path('is-up-to-date/', IsUpToDate.as_view())
 ]
--- level/views.py
+++ level/views.py
@@ -5,9 +5,20 @@ from .serializers import LevelSerializer
 from rest_framework.response import Response
 import hashlib 
 import json
-
+from rest_framework import status
 # encoding GeeksforGeeks using md5 hash 
-# function  
+# function
+
+
+def get_latest_hash():
+    all_levels = Level.objects.all()
+
+    data = LevelSerializer(all_levels, many=True).data
+    data_hash = hashlib.md5(json.dumps(data).encode('utf-8'))
+
+    return str(data_hash.hexdigest())
+
+
 class ListLevelsView(GenericAPIView):
     def get_queryset(self):
         pass
@@ -27,7 +38,17 @@ class IsUpToDate(GenericAPIView):
     def get_queryset(self):
         pass
     
+    def get(self, request):
+        hash = request.GET.get('hash', None)
+
+        if not hash:
+            return Response({
+                'Error': 'No Hash'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({'is_up_to_date': hash == get_latest_hash()})
     
+
 
 
 

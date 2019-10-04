88f807dc67477964eb0b1d05fef17ce9fb180d6c
Ali Parvizi <snparvizi75@gmail.com>
2019-10-04 10:17:13 +0330

    pack urls

--- level/urls.py
+++ level/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import ListLevelsView, IsUpToDate
+from .views import ListLevelsView, IsUpToDate, ListPacksView, ListUserPackage, AddUserPack
+
 urlpatterns = [
     path('all-levels/', ListLevelsView.as_view()),
+    path('all-packs/', ListPacksView.as_view()),
+    path('user-packs/', ListUserPackage.as_view()),
+    path('add-user-pack/', AddUserPack.as_view()),
     path('is-up-to-date/', IsUpToDate.as_view())
 ]

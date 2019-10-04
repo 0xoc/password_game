628f88833901e445bea143046feb1fd7e8a70e52
Ali Parvizi <snparvizi75@gmail.com>
2019-10-04 10:39:53 +0330

    pakage user relation

--- level/admin.py
+++ level/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import *
 
+
+class LevelPackageAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'price']
+
+
+class PackageUserRelationAdmin(admin.ModelAdmin):
+    list_display = ['user_profile', 'package__name', 'package__price']
+
+
 admin.site.register(Level)
-admin.site.register(LevelPackage)
-admin.site.register(PackageUserRelation)
+admin.site.register(LevelPackage,  LevelPackageAdmin)
+admin.site.register(PackageUserRelation, PackageUserRelationAdmin)
 admin.site.register(UserProfile)

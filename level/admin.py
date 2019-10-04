46ab2424dc983591ac84680c0c47b84d743c770e
Ali Parvizi <snparvizi75@gmail.com>
2019-10-04 10:33:18 +0330

    admin registers

--- level/admin.py
+++ level/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Level
+from .models import *
 
 admin.site.register(Level)
+admin.site.register(LevelPackage)
+admin.site.register(PackageUserRelation)
+admin.site.register(UserProfile)
